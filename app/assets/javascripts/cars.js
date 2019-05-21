88d5f6b977891c1dee1c4dc4ef07c005eb114062
Jiří Lichnovský <lichny@Jiri-Mac-mini.local>
2019-05-21 17:14:56 +0200

    Add seed

--- app/assets/javascripts/cars.js
+++ app/assets/javascripts/cars.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var url = new URL(document.URL);
-  var name = url.href.split('?')[1].split('&')[0].split("=")
-  if(name[0] == "name"){
-    var neco = document.getElementById("name").html = "Vítejte "+name[1];
+  var name = url.searchParams.get("name")
+  //var neco = document.getElementById("name")
+  if(name!="" && name!=null){
+    document.write("Vítejte "+name);
   }
-  debugger
 })
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([{'login' => 'admin', 'password' => '123', 'admin'=>true},{'login' => 'customer', 'password' => '123', 'admin'=>false}])
+Car.create!([{'brand' => 'Škoda', 'name' => 'Felicia', 'color'=>'Red', 'manufacturing_date'=>'', 'spz'=>'NJK 14-25', 'mileage'=>254122},
+  {'brand' => 'Volkswagen', 'name' => 'Golf', 'color'=>'Silver', 'manufacturing_date'=>'', 'spz'=>'2T9 9852', 'mileage'=>147122},
+  {'brand' => 'Seat', 'name' => 'Alhambra', 'color'=>'Green', 'manufacturing_date'=>'', 'spz'=>'NJL 17-85', 'mileage'=>547413},
+  {'brand' => 'Kia', 'name' => 'Ceed', 'color'=>'Blue', 'manufacturing_date'=>'', 'spz'=>'6T7 4697', 'mileage'=>10545},
+  {'brand' => 'Fiat', 'name' => 'Multipla', 'color'=>'Pink', 'manufacturing_date'=>'', 'spz'=>'NJA 01-25', 'mileage'=>254122}
+])
